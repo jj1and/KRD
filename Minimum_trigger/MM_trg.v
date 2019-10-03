089bed285c5a07806e4169be2d2549be4eab3f8c
jj1and <kosuke@awa.tohoku.ac.jp>
2019-10-03 20:08:06 +0900

    triggerd_flag(旧start_trg)に一本化

--- Minimum_trigger/MM_trg.v
+++ Minimum_trigger/MM_trg.v
@@ -8,6 +8,9 @@ module MM_trg # (
     // acquiasion length settings
     parameter integer POST_ACQUI_LEN = 76/2,
 
+    // acquiasion length settings
+    parameter integer ACQUI_LEN = 200/2,
+
     // TIME STAMP DATA WIDTH
     parameter integer TIME_STAMP_WIDTH = 16,
 
@@ -31,10 +34,8 @@ module MM_trg # (
     // triggered time stamp
 	output wire [TIME_STAMP_WIDTH-1:0] O_TIME_STAMP,
 
-    // trigger start output
-    output wire O_START_TRG,
-    // trigger start output
-    output wire O_FINALIZE_TRG,
+    // triggered flag output
+    output wire O_TRIGGERD_FLAG,
 
     // Ports of Axi Slave Bus Interface S00_AXIS　
     input wire  AXIS_ACLK,
@@ -53,6 +54,7 @@ module MM_trg # (
     endfunction
 
     localparam integer POST_COUNTER_WIDTH = clogb2(POST_ACQUI_LEN-1); 
+    localparam integer FULL_COUNTER_WIDTH = clogb2(ACQUI_LEN-1);
     localparam integer SAMPLE_PER_TDATA = S_AXIS_TDATA_WIDTH/16;
     localparam integer REDUCE_DIGIT = 16 - ADC_RESOLUTION_WIDTH;
     localparam integer THRESHOLD_VAL = (2**ADC_RESOLUTION_WIDTH-1)*THRESHOLD/100;
@@ -68,14 +70,25 @@ module MM_trg # (
     reg [1:0] adc_val_state = ZONE_0;
     reg [1:0] adc_val_state_delay = ZONE_0;
 
-    // start trigger flag
-    reg start_trg;
+    // triggered flag 
+    reg triggerd_flag;
+    assign TRIGGERD_FLAG = triggerd_flag;
 
-    // finalize trigger flag
+    // finalizing trigger
     reg finalize_trg;
+
+    // finish trigger flag
+    reg finish_trg;
+
+    // ACQUI_LEN 内にtriggerが終わらなかった場合のフラグ
+    reg over_len_flag;
+
 
     // POST ACQUIASION COUNTER
     reg [POST_COUNTER_WIDTH-1:0] post_count = 0;
+
+    // ACQUASION COUNTER
+    reg [FULL_COUNTER_WIDTH-1:0] full_count = 0;
 
     // comparing ADC value with THRESHOLD_VAL
     wire [SAMPLE_PER_TDATA-1:0] compare_result;
@@ -119,15 +132,14 @@ module MM_trg # (
     begin  
     if (!AXIS_ARESETN) 
         begin
-        start_trg <= 1'b0;
-        finalize_trg <= 1'b0;
+        triggerd_flag <= 1'b0;
         time_stamp <= 0;
         end
     else
         begin
         if (adc_val_state == ZONE_1)
             begin
-            start_trg <= 1'b1;
+            triggerd_flag <= 1'b1;
             time_stamp <= CURRENT_TIME;
             end
         else
@@ -135,20 +147,19 @@ module MM_trg # (
             time_stamp <= time_stamp;
             if (adc_val_state_delay == ZONE_1)
                 begin
-                start_trg <= 1'b1;
                 finalize_trg <= 1'b1;
                 end
             else
                 begin
-                if ((post_count>0)&(post_count<POST_ACQUI_LEN))
+                if (finish_trg|over_len_flag)
                     begin
-                    start_trg <= 1'b1;
-                    finalize_trg <= 1'b1;
+                    triggerd_flag <= 1'b0;
+                    finalize_trg <= 1'b0;
                     end
                 else
                     begin
-                    start_trg <= 1'b0;
-                    finalize_trg <= 1'b0;                      
+                    triggerd_flag <= 1'b1;
+                    finalize_trg <= 1'b1;                      
                     end
                 end
             end
@@ -160,27 +171,62 @@ module MM_trg # (
     begin  
     if (!AXIS_ARESETN) 
         begin
+        finish_trg <= 1'b1;
         post_count <= 0;
         end
     else
         begin
         if (finalize_trg)
             begin
-            if (post_count>POST_ACQUI_LEN-1)
+            if (post_count>=POST_ACQUI_LEN-1)
                 begin
+                finish_trg <= 1'b1;
                 post_count <= 0;
                 end
             else 
                 begin
+                finish_trg <= 1'b0;
                 post_count <= post_count + 1;
                 end
             end
         else
             begin
+            finish_trg <= 1'b1;
             post_count <= 0;
             end        
         end
     end
+
+    // ACQUI_LEN のカウント
+    always @(posedge AXIS_ACLK )
+    begin
+      if (!AXIS_ARESETN)
+        begin
+        over_len_flag <= 1'b0;
+        full_count <= 0;
+        end
+      else
+        begin
+          if (triggerd_flag)
+            begin
+              if ( full_count >= ACQUI_LEN-POST_ACQUI_LEN-1 )
+                begin
+                over_len_flag <= 1'b1;
+                full_count <= 0;  
+                end
+              else
+                begin
+                  over_len_flag <= 1'b0;
+                  full_count <= full_count + 1;
+                end
+            end
+          else
+            begin
+            over_len_flag <= 1'b0;
+            full_count <= 0;
+            end
+        end
+    end  
 
     // Thresoldの値との比較
     genvar i;
@@ -192,7 +238,7 @@ module MM_trg # (
     endgenerate
 
     assign O_TIME_STAMP = time_stamp;
-    assign O_START_TRG = start_trg;
+    assign O_TRIGGERD_FLAG = triggerd_flag;
     assign O_FINALIZE_TRG = finalize_trg;
 
 
