450ad8b03fe3dfa82bd197b4f9acea5792fe6814
jj1and <kosuke@awa.tohoku.ac.jp>
2019-10-28 18:39:33 +0900

    time counterのコードの最適化

--- Minimum_trigger/time_counter.v
+++ Minimum_trigger/time_counter.v
@@ -22,75 +22,59 @@ module time_counter # (
     input wire  AXIS_ARESETN,
     
     // current time
-    output wire [TIME_STAMP_WIDTH-1:0] O_CURRENT_TIME
+    output wire [TIME_STAMP_WIDTH-1:0] CURRENT_TIME
 );
 
+  // function called clogb2 that returns an integer which has the 
+  // value of the ceiling of the log base 2.
+  function integer clogb2 (input integer bit_depth); begin
+    for(clogb2=0; bit_depth>0; clogb2=clogb2+1)
+      bit_depth = bit_depth >> 1;
+    end
+  endfunction
+
   localparam integer MAX_TIME_COUNT = 2**TIME_STAMP_WIDTH-1;
-  localparam integer DIVIDE_NUM = AXIS_ACLK_FREQ/TIMER_RESO_FREQ;
+  localparam integer CLK_DIVIDE = AXIS_ACLK_FREQ/TIMER_RESO_FREQ;
+  localparam integer CLK_DIVIDE_WIDTH = clogb2(CLK_DIVIDE-1);
 
   //  exec state
   localparam [1:0] INIT = 2'b00, // ADC < THRESHOLD_VAL
                   TRG = 2'b11; // ADC > THRESHOLD_VAL
 
   // enable counter
-  reg [DIVIDE_NUM-1:0] en_cnt;
-
+  reg [CLK_DIVIDE_WIDTH-1:0] divide_clk;
   // timer enable
-  reg time_en;
-
+  wire counter_en;
+  wire time_incr;
   // current time
   reg [TIME_STAMP_WIDTH-1:0] current_time = 0;
-  assign O_CURRENT_TIME = current_time;
+  wire [TIME_STAMP_WIDTH-1:0] current_timeD;
+  
+  assign time_incr = (divide_clk == CLK_DIVIDE-1);
+  assign current_timeD = current_time + {{TIME_STAMP_WIDTH-1{1'b0}}, time_incr};
+  assign counter_en = (EXEC_STATE != INIT);
+  assign CURRENT_TIME = current_time;
 
-  // time counter の動作
+  // time counter
   always @(posedge AXIS_ACLK ) begin
-    if (!AXIS_ARESETN) begin
-      current_time <= 1;
+    if (!AXIS_ARESETN || (!counter_en)) begin
+      current_time <= 0;
     end else begin
-      if (time_en) begin
-        if (current_time > MAX_TIME_COUNT-1) begin
-          current_time <= 1;
-        end else begin
-          if (en_cnt == 0) begin
-            current_time <= current_time + 1;
-          end else begin
-            current_time <= current_time;
-          end
-        end
-      end else begin
-        current_time <= 1;
-      end
+      current_time <= current_timeD;
     end
   end
 
-  // en_cnt の動作(clockの分周のようなもの)
+  // clock divide
   always @(posedge AXIS_ACLK ) begin
     if (!AXIS_ARESETN) begin
-      en_cnt <= 0;
+       divide_clk <= 0;
     end else begin
-      if (time_en) begin
-        if (en_cnt>DIVIDE_NUM-1) begin
-          en_cnt <= 0; 
-        end else begin
-          en_cnt <= en_cnt + 1;
-        end
+      if (divide_clk >= CLK_DIVIDE-1) begin
+        divide_clk <= 0;
       end else begin
-        en_cnt <= en_cnt;
+        divide_clk <= divide_clk + 1;
       end
     end
   end
-
-  // time enable の動作
-  always @(posedge AXIS_ACLK ) begin
-    if (!AXIS_ARESETN) begin
-      time_en <= 1'b0;
-    end else begin
-      if (EXEC_STATE==INIT) begin
-        time_en <= 1'b0;
-      end else begin
-        time_en <= 1'b1;
-      end
-    end
-  end  
 
 endmodule
