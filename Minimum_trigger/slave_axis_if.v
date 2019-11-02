e8c37fc7bde75aeed2d440dc38e3e58c6cc6e3ff
jj1and <kosuke@awa.tohoku.ac.jp>
2019-11-02 19:43:11 +0900

    データをスルー

--- Minimum_trigger/slave_axis_if.v
+++ Minimum_trigger/slave_axis_if.v
@@ -3,36 +3,42 @@
 module slave_axis_if # (
   parameter integer S_AXIS_TDATA_WIDTH = 128
 )(
-  input wire CLK,
+  input wire AXIS_ACLK,
+  input wire AXIS_ARESETN,
   input wire [S_AXIS_TDATA_WIDTH-1:0] S_AXIS_TDATA,
   input wire S_AXIS_TVALID,
   output wire S_AXIS_TREADY,
 
+  output wire CLK,
+  output wire RESETN,
   output wire [S_AXIS_TDATA_WIDTH-1:0] TDATA,
   output wire TVALID,
-  input wire BUFF_WRITE_READY,
-  input wire TRIMMER_READY
+  input wire ALL_MODULE_READY
 );
 
-wire s_axis_treadyD;
-reg s_axis_tready;
-wire s_axis_tvalidD;
-reg tvalid;
-wire [S_AXIS_TDATA_WIDTH-1:0] s_axis_tdataD;
-reg [S_AXIS_TDATA_WIDTH-1:0] tdata;
+// wire s_axis_treadyD;
+// reg s_axis_tready;
+// wire s_axis_tvalidD;
+// reg tvalid;
+// wire [S_AXIS_TDATA_WIDTH-1:0] s_axis_tdataD;
+// reg [S_AXIS_TDATA_WIDTH-1:0] tdata;
 
-assign s_axis_treadyD = BUFF_WRITE_READY&TRIMMER_READY;
-assign s_axis_tdataD = S_AXIS_TDATA;
-assign s_axis_tvalidD = S_AXIS_TVALID;
+// assign s_axis_treadyD = BUFF_WRITE_READY&TRIMMER_READY;
+// assign s_axis_tdataD = S_AXIS_TDATA;
+// assign s_axis_tvalidD = S_AXIS_TVALID;
 
-assign TDATA = tdata;
-assign TVALID = tvalid;
+assign CLK = AXIS_ACLK;
+assign RESETN = AXIS_ARESETN;
+assign TDATA = S_AXIS_TDATA;
+assign TVALID = S_AXIS_TVALID;
+assign S_AXIS_TREADY = ALL_MODULE_READY;
 
-always @(posedge CLK ) begin
-  tdata <= s_axis_tdataD;
-  s_axis_tready <= s_axis_treadyD;
-  tvalid <= s_axis_tvalidD;
-end
+// Bellow code violated to timing constrains
+// always @(posedge CLK ) begin
+//   tdata <= s_axis_tdataD;
+//   s_axis_tready <= s_axis_treadyD;
+//   tvalid <= s_axis_tvalidD;
+// end
 
 
 endmodule
