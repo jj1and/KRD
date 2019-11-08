8f662d1f32890313712869d79a8e488f739d8bfc
jj1and <kosuke@awa.tohoku.ac.jp>
2019-11-08 20:57:50 +0900

    桁を分けることでタイミング違反を解決する

--- Minimum_trigger/led_chika.v
+++ Minimum_trigger/led_chika.v
@@ -17,45 +17,100 @@ module led_chika # (
     end
   endfunction
 
-  localparam integer COUNT_MAX = CLOCK_FREQ_MHZ*INTERVAL_MSEC*(10**3);
+  // localparam integer COUNT_MAX = CLOCK_FREQ_MHZ*INTERVAL_MSEC*(10**3);
+  // for test
+  localparam integer COUNT_MAX = CLOCK_FREQ_MHZ*INTERVAL_MSEC;
+  
   localparam integer COUNT_WIDTH = clogb2(COUNT_MAX-1);
+  localparam integer TOP_BITS = COUNT_WIDTH%8;
+  localparam integer NUM_OF_8BIT = (COUNT_WIDTH-TOP_BITS)/8;
 
-  reg [COUNT_WIDTH-1:0] count;
+  wire [NUM_OF_8BIT*8-1:0] lower_count;
+  wire [COUNT_WIDTH-1:0] count;
+  reg [7:0] lower_bits[NUM_OF_8BIT-1:0];
+  reg [TOP_BITS-1:0] top_bits;
+  reg [0:0] shift_up[NUM_OF_8BIT-1:0];
+  wire [0:0] shift_upD[NUM_OF_8BIT-1:0];
   reg led_out;
   reg blink;
   wire blinkD;
 
+  assign count = {top_bits, lower_count};
   assign blinkD = (count == COUNT_MAX-2);
   assign LED_OUT = led_out;
 
+  genvar i;
+  generate
+    for ( i=0 ; i<NUM_OF_8BIT ; i=i+1 ) begin
+      assign shift_upD[i] = (lower_bits[i] == {{7{1'b1}}, 1'b0});
+      assign lower_count[8*i +:8] = lower_bits[i];
+    end
+  endgenerate
+
+  generate
+    for ( i=0 ; i<NUM_OF_8BIT ; i=i+1 ) begin
+      always @(posedge CLK) begin
+        if (!RESETN) begin
+          shift_up[i] <= #10 0;
+        end else begin
+          shift_up[i] <= #10 shift_upD[i];
+        end
+      end
+    end
+  endgenerate
+
+  generate
+    for ( i=1 ; i<NUM_OF_8BIT ; i=i+1 ) begin
+      always @(posedge CLK ) begin
+        if ((!RESETN)|blink) begin
+          lower_bits[i] <= #10 0;
+        end else begin
+          if (shift_up[i-1]) begin
+            lower_bits[i] <= #10 lower_bits[i] + 1;
+          end else begin
+            lower_bits[i] <= #10 lower_bits[i];
+          end
+        end
+      end
+    end
+  endgenerate
+
   always @(posedge CLK ) begin
-    if (!RESETN) begin
-      count <= 0;
+    if ((!RESETN)|blink) begin
+      lower_bits[0] <= #10 0;
     end else begin
-      if (count == COUNT_MAX-1) begin
-        count <= 0;
+      lower_bits[0] <= #10 lower_bits[0] + 1;
+    end
+  end
+
+  always @(posedge CLK ) begin
+    if ((!RESETN)|blink) begin
+      top_bits <= #10 0;
+    end else begin
+      if (shift_up[NUM_OF_8BIT-1]) begin
+        top_bits <= #10 top_bits + 1;
       end else begin
-        count <= count + 1;
+        top_bits <= #10 top_bits;
       end
     end
   end
 
-  always @( CLK ) begin
+  always @(posedge CLK ) begin
     if (!RESETN) begin
-      blink <= 1'b1;
+      blink <= #10 1'b1;
     end else begin
-      blink <= blinkD;
+      blink <= #10 blinkD;
     end
   end
 
-  always @( CLK ) begin
+  always @(posedge CLK ) begin
     if (!RESETN) begin
-      led_out <= 1'b1;
+      led_out <= #10 1'b1;
     end else begin
       if (blink) begin
-        led_out <= 1'b1;
+        led_out <= #10 ~led_out;
       end else begin
-        led_out <= 1'b0;
+        led_out <= #10 led_out;
       end
     end
   end
---
+++ Minimum_trigger/led_chika_tb.v
@@ -0,0 +1,48 @@
+`timescale 1 ps / 1 ps
+
+module led_chika_tb;
+
+  parameter integer CLK_FREQ_MHz = 500;
+  parameter integer INTERVAL_MSEC = 1;
+
+  parameter integer CLK_PERIOD = (10**6)/CLK_FREQ_MHz;
+
+  reg clk;
+  reg resetn = 1'b0;
+  wire led_out;
+
+  // ------ clock generation ------
+  initial
+  begin
+      clk = 1'b0;
+  end
+
+  always #( CLK_PERIOD/2 )
+  begin
+    clk <= ~clk;
+  end
+
+  // ------ DUT ------
+  led_chika # (
+    .CLOCK_FREQ_MHZ(CLK_FREQ_MHz),
+    .INTERVAL_MSEC(INTERVAL_MSEC)
+  ) DUT (
+    .CLK(clk),
+    .RESETN(resetn),
+    .LED_OUT(led_out)
+  );
+
+  // ------ test-bench ------
+  initial begin
+    $dumpfile("led_chika_tb.vcd");
+    $dumpvars(0, led_chika_tb);
+
+    resetn <= 1'b0;
+    repeat(10) @(posedge clk);
+    resetn <= 1'b1;
+    repeat(1000) @(posedge clk);
+
+    $finish;
+  end
+
+endmodule
