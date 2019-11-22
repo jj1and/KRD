6db4a414a85b7a9ad47b302373ae75cfcee79466
jj1and <kosuke@awa.tohoku.ac.jp>
2019-11-22 12:49:47 +0900

    xilinx built-in FIFO用に仕様変更(最小delayは3clock)

--- common_source/Variable_delay_mod.v
+++ common_source/Variable_delay_mod.v
@@ -17,6 +17,7 @@ module Variable_delay_mod # (
   input wire [WIDTH-1:0] FIFO_DOUT,
   input wire FIFO_NOT_EMPTY,
   input wire FIFO_FULL,
+  input wire FIFO_RST_BUSY,
   output wire DELAY_READY,
   output wire DELAY_VALID
 );
@@ -33,31 +34,51 @@ module Variable_delay_mod # (
   wire not_empty;
 
   reg [MAX_DELAY_CNT_WIDTH-1:0] delay_cnt;
+  reg read_ready;
   reg [MAX_DELAY_CNT_WIDTH-1:0] delay;
   reg write_en;
+  wire write_enD;
   reg read_en;
+  wire read_enD;
   reg valid;
   assign DELAY_VALID = valid;
   assign DELAY_READY = write_en;
   assign FIFO_DIN = DIN;
   assign FIFO_WE = write_en;
   assign FIFO_RE = read_en;
+  
+  assign write_enD = (!FIFO_FULL)&(!FIFO_RST_BUSY);
+  assign read_enD = (FIFO_NOT_EMPTY)&(read_ready);
   assign not_empty = FIFO_NOT_EMPTY;
   assign full = FIFO_FULL;
+
   assign DOUT = FIFO_DOUT;
   
   always @(posedge CLK ) begin
     if (!RESETN) begin
-      read_en <= #400 1'b0;
       delay_cnt <= #400 0;
       delay <= #400 DELAY;
     end else begin
-      if (delay_cnt >= delay) begin
-        delay_cnt <= #400 delay;
-        read_en <= #400 1'b1;
+      if (delay_cnt >= delay-2) begin
+        delay_cnt <= #400 delay-1;
       end else begin
-        delay_cnt <= #400 delay_cnt + 1;
-        read_en <= #400 1'b0;
+        if (write_enD) begin
+          delay_cnt <= #400 delay_cnt + 1;
+        end else begin
+          delay_cnt <= #400 0;
+        end
+      end
+    end
+  end
+
+  always @(posedge CLK ) begin
+    if (!RESETN) begin
+      read_ready <= #400 1'b0;
+    end else begin
+      if (delay_cnt>=delay-2) begin
+        read_ready <= #400 1'b1;
+      end else begin
+        read_ready <= #400 1'b0;
       end
     end
   end
@@ -65,12 +86,10 @@ module Variable_delay_mod # (
   always @(posedge CLK) begin
     if (!RESETN) begin
       write_en <= #400 1'b0;
+      read_en <= #400 1'b0;
     end else begin
-      if (!full) begin
-        write_en <= #400 1'b1;
-      end else begin
-        write_en <= #400 1'b0;
-      end
+      write_en <= #400 write_enD;
+      read_en <= #400 read_enD;
     end
   end
 
@@ -78,7 +97,7 @@ module Variable_delay_mod # (
     if (!RESETN) begin
       valid <= #400 1'b0;
     end else begin
-      if (not_empty && (delay_cnt >= delay)) begin
+      if (read_en) begin
         valid <= #400 1'b1;
       end else begin
         valid <= #400 1'b0;
