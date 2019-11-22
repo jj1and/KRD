4c13413b141ed4bc6ab0345acb6dbbd09c43452b
jj1and <kosuke@awa.tohoku.ac.jp>
2019-11-22 12:48:18 +0900

    xilinx built-in FIFOと同じ挙動に修正

--- common_source/Fifo.v
+++ common_source/Fifo.v
@@ -34,6 +34,7 @@ module Fifo # (
   integer i;
 
   reg [WIDTH-1:0] sram[ACTUAL_DEPTH-1:0];
+  reg [WIDTH-1:0] dout;
   
   wire [DEPTH_BIT_WIDTH:0] next_wp;
   wire [DEPTH_BIT_WIDTH:0] wpD;
@@ -43,17 +44,11 @@ module Fifo # (
   wire [DEPTH_BIT_WIDTH:0] rpD;
   reg [DEPTH_BIT_WIDTH:0] rp;
 
-  wire not_emptyD;
-  reg not_empty;
-
-  wire fullD;
-  reg full;
-
   wire wp_inc;
   wire rp_inc;
    
-  assign wp_inc = WE&(!full);
-  assign rp_inc = RE&not_empty;
+  assign wp_inc = WE&(!FULL);
+  assign rp_inc = RE&NOT_EMPTY;
 
   assign wpD = wp + {{ACTUAL_DEPTH-1{1'b0}}, wp_inc};
   assign rpD = rp + {{ACTUAL_DEPTH-1{1'b0}}, rp_inc};
@@ -61,18 +56,22 @@ module Fifo # (
   assign next_wp = wp + 1;
   assign next_rp = rp + 1; 
 
-  assign fullD = ( (next_wp[DEPTH_BIT_WIDTH] != rp[DEPTH_BIT_WIDTH]) && (next_wp[DEPTH_BIT_WIDTH-1:0] == rp[DEPTH_BIT_WIDTH-1:0]) );
-  assign not_emptyD = (wp != rp);
-
-  assign NOT_EMPTY = not_empty;
-  assign FULL = full;
+  assign NOT_EMPTY = (wp != rp);
+  assign FULL = ( (next_wp[DEPTH_BIT_WIDTH] != rp[DEPTH_BIT_WIDTH]) && (next_wp[DEPTH_BIT_WIDTH-1:0] == rp[DEPTH_BIT_WIDTH-1:0]) );
 
   assign PROGRAMMABLE_EMPTY = ((wp-rp)<=PROG_EMPTY_THRE);
   assign PROGRAMMABLE_FULL = ((wp-rp)>=PROG_FULL_THRE);
 
-  assign DOUT = sram[rp[DEPTH_BIT_WIDTH-1:0]];
+  assign DOUT = dout;
+ 
+  always @(posedge CLK) begin
+    if (!RESETN) begin
+      dout <= #400 INIT_VALUE;
+    end else begin
+      dout <= #400 sram[rp[DEPTH_BIT_WIDTH-1:0]];
+    end
+  end
 
-  
   always @(posedge CLK ) begin
     if (!RESETN) begin
       for ( i=0 ; i<ACTUAL_DEPTH ; i=i+1 ) begin
@@ -80,22 +79,12 @@ module Fifo # (
         end      
     end else begin
       if (wp_inc) begin
-        sram[wp[DEPTH_BIT_WIDTH-1:0]] <= DIN; 
+        sram[wp[DEPTH_BIT_WIDTH-1:0]] <= #400 DIN; 
       end else begin
         for ( i=0 ; i<ACTUAL_DEPTH ; i=i+1 ) begin
             sram[i] <= #400 sram[i];
           end
       end      
-    end
-  end
-
-  always @(posedge CLK ) begin
-    if (!RESETN) begin
-      full <= #400 1'b0;
-      not_empty <= #400 1'b0;
-    end else begin
-      full <= #400 fullD;
-      not_empty <= #400 not_emptyD;
     end
   end
 
