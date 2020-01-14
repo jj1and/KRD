b016c6c917ddbbf32bce54664b27b7efe17b2c83
jj1and <kosuke@awa.tohoku.ac.jp>
2020-01-14 11:09:58 +0900

    updated mixer ip for changing data frame

--- ip_repo/TwoChMixer/src/TwoChMixer_mod.v
+++ ip_repo/TwoChMixer/src/TwoChMixer_mod.v
@@ -23,7 +23,8 @@ module TwoChMixer_mod # (
   output wire oVALID
 );
 
-  localparam integer HEADER_FOOTER_ID_WIDTH = 8;
+  localparam integer HEADER_FOOTER_ID_WIDTH = 16;
+  localparam integer ID_WIDTH_HEX = HEADER_FOOTER_ID_WIDTH/4;
   
   reg [DATA_WIDTH-1:0] ch0_din;
   reg [DATA_WIDTH-1:0] ch0_header_checked;
@@ -63,30 +64,35 @@ module TwoChMixer_mod # (
   reg sending;
   reg [DATA_WIDTH-1:0] dout;
 
-  // 8'hFE or 8'hFF = 7'b1111111
-  assign CH0_DIN_header_foundD = (CH0_DIN[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}});
-  assign CH1_DIN_header_foundD = (CH1_DIN[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}});
-  assign ch0_footer_checked_header_foundD = (ch0_footer_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}});
-  assign ch1_footer_checked_header_foundD = (ch1_footer_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}});
+  reg [15:0] HEADER_ID = 16'hAAAA;
+  reg [15:0] FOOTER_ID = 16'h5555;
+  reg [15:0] ERROR_HEADER_ID = 16'hAAEE;
+  reg [15:0] ERROR_FOOTER_ID = 16'h55EE;
 
   // 8'hFE or 8'hFF = 7'b1111111
-  assign ch0_header_lostD = (ch0_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]!={7{1'b1}})&(ch0_head_foot_id_hist==3'b110);
-  assign ch1_header_lostD = (ch1_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]!={7{1'b1}})&(ch1_head_foot_id_hist==3'b110);  
+  assign CH0_DIN_header_foundD = (CH0_DIN[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH]==HEADER_ID);
+  assign CH1_DIN_header_foundD = (CH1_DIN[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH]==HEADER_ID);
+  assign ch0_footer_checked_header_foundD = (ch0_footer_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH]==HEADER_ID);
+  assign ch1_footer_checked_header_foundD = (ch1_footer_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH]==HEADER_ID);
+
+  // 8'hFE or 8'hFF = 7'b1111111
+  assign ch0_header_lostD = (ch0_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH]!=HEADER_ID)&(ch0_head_foot_id_hist==3'b110);
+  assign ch1_header_lostD = (ch1_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH]!=HEADER_ID)&(ch1_head_foot_id_hist==3'b110);  
 
   // 8'hEF or 8'h0F = 5'b01111
-  assign CH0_DIN_footer_foundD = (CH0_DIN[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(CH0_DIN[DATA_WIDTH-1 -:2]==2'b11);
-  assign CH1_DIN_footer_foundD = (CH1_DIN[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(CH1_DIN[DATA_WIDTH-1 -:2]==2'b11);
-  assign ch0_correct_data_footer_foundD = (ch0_correct_data[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(ch0_correct_data[DATA_WIDTH-1 -:2]==2'b11);
-  assign ch1_correct_data_footer_foundD = (ch1_correct_data[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(ch1_correct_data[DATA_WIDTH-1 -:2]==2'b11);  
+  assign CH0_DIN_footer_foundD = (CH0_DIN[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);
+  assign CH1_DIN_footer_foundD = (CH1_DIN[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);
+  assign ch0_correct_data_footer_foundD = (ch0_correct_data[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);
+  assign ch1_correct_data_footer_foundD = (ch1_correct_data[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);  
 
   // 8'hFE or 8'hFF = 7'b1111111
-  assign ch0_footer_lostD = (ch0_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}})&((ch0_head_foot_id_hist & 3'b101)==3'b101);
-  assign ch1_footer_lostD = (ch1_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}})&((ch1_head_foot_id_hist & 3'b101)==3'b101);
+  assign ch0_footer_lostD = (ch0_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]==HEADER_ID)&((ch0_head_foot_id_hist & 3'b101)==3'b101);
+  assign ch1_footer_lostD = (ch1_din[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]==HEADER_ID)&((ch1_head_foot_id_hist & 3'b101)==3'b101);
 
-  assign ch0_header_checked_pauseD = (ch0_header_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}})|((ch0_header_checked[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(ch0_header_checked[DATA_WIDTH-1 -:2]==2'b11));
-  assign ch1_header_checked_pauseD = (ch1_header_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]=={7{1'b1}})|((ch1_header_checked[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(ch1_header_checked[DATA_WIDTH-1 -:2]==2'b11));  
-  assign ch0_footer_checked_pauseD = (ch0_footer_checked[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(ch0_footer_checked[DATA_WIDTH-1 -:2]==2'b11);
-  assign ch1_footer_checked_pauseD = (ch1_footer_checked[HEADER_FOOTER_ID_WIDTH-4:0]==5'b01111)&(ch1_footer_checked[DATA_WIDTH-1 -:2]==2'b11);      
+  assign ch0_header_checked_pauseD = (ch0_header_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]==HEADER_ID)|(ch0_header_checked[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);
+  assign ch1_header_checked_pauseD = (ch1_header_checked[DATA_WIDTH-1 -:HEADER_FOOTER_ID_WIDTH-1]==HEADER_ID)|(ch1_header_checked[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);  
+  assign ch0_footer_checked_pauseD = (ch0_footer_checked[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);
+  assign ch1_footer_checked_pauseD = (ch1_footer_checked[HEADER_FOOTER_ID_WIDTH-1:0]==FOOTER_ID);      
 
   assign CH0_RE = ch0_re;
   assign CH1_RE = ch1_re;
@@ -120,7 +126,7 @@ module TwoChMixer_mod # (
       if ((CH0_DIN_footer_foundD|ch0_footer_lostD)) begin
         ch0_re <= #400 1'b0;        
       end else begin
-        if ((CH0_READ_REQUEST& iREADY)&(!sending)) begin
+        if ((CH0_READ_REQUEST& iREADY)&(!ch1_re)) begin
           ch0_re <= #400 1'b1;
         end else begin
           ch0_re <= #400 ch0_re;
@@ -135,7 +141,7 @@ module TwoChMixer_mod # (
     end else begin
       if (ch0_re) begin
         if (ch0_header_lostD) begin
-          ch0_din <= #400 {8'hFE, {DATA_WIDTH-HEADER_FOOTER_ID_WIDTH-8{1'b1}}, 8'b0};
+          ch0_din <= #400 {ERROR_HEADER_ID, {DATA_WIDTH-HEADER_FOOTER_ID_WIDTH{1'b1}}};
         end else begin
           ch0_din <= #400 CH0_DIN;
         end
@@ -165,7 +171,7 @@ module TwoChMixer_mod # (
         ch0_footer_checked <= #400 ch0_footer_checked;
       end else begin
         if (ch0_footer_lostD) begin
-          ch0_footer_checked <= #400 {4'b1100, {DATA_WIDTH-12{1'b1}}, 8'hEF};
+          ch0_footer_checked <= #400 {{DATA_WIDTH-HEADER_FOOTER_ID_WIDTH{1'b1}}, ERROR_FOOTER_ID};
         end else begin
           ch0_footer_checked <= #400 ch0_header_checked;
         end
@@ -225,7 +231,7 @@ module TwoChMixer_mod # (
       if (CH1_DIN_footer_foundD|ch1_footer_lostD) begin
         ch1_re <= #400 1'b0;
       end else begin
-        if (((read_request==2'b10)& iREADY)&(!sending)) begin
+        if (((read_request==2'b10)& iREADY)&(!ch0_re)) begin
           ch1_re <= #400 1'b1;
         end else begin
           ch1_re <= #400 ch1_re;
@@ -240,7 +246,7 @@ module TwoChMixer_mod # (
     end else begin
       if (ch1_re) begin
         if (ch1_header_lostD) begin
-          ch1_din <= #400 {8'hFE, {DATA_WIDTH-HEADER_FOOTER_ID_WIDTH-8{1'b1}}, 8'b0};
+          ch1_din <= #400 {ERROR_HEADER_ID, {DATA_WIDTH-HEADER_FOOTER_ID_WIDTH{1'b1}}};
         end else begin
             ch1_din <= #400 CH1_DIN;
         end
@@ -270,7 +276,7 @@ module TwoChMixer_mod # (
         ch1_footer_checked <= #400 ch1_footer_checked;
       end else begin
         if (ch1_footer_lostD) begin
-          ch1_footer_checked <= #400 {4'b1100, {DATA_WIDTH-12{1'b1}}, 8'hEF};
+          ch1_footer_checked <= #400 {{DATA_WIDTH-HEADER_FOOTER_ID_WIDTH{1'b1}}, ERROR_FOOTER_ID};
         end else begin
           ch1_footer_checked <= #400 ch1_header_checked;
         end
