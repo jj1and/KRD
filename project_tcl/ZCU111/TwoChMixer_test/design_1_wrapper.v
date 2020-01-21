48c49a2a7d2da0769f4b3b821e8595cb0f63ce3c
jj1and <kosuke@awa.tohoku.ac.jp>
2020-01-21 15:58:43 +0900

    Mixer behavior is confirmed but  found Trigger bug

--- project_tcl/ZCU111/TwoChMixer_test/design_1_wrapper.v
+++ project_tcl/ZCU111/TwoChMixer_test/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Tue Dec 24 21:44:28 2019
+//Date        : Mon Jan 20 23:01:01 2020
 //Host        : Akabeko running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
@@ -10,11 +10,10 @@
 `timescale 1 ps / 1 ps
 
 module design_1_wrapper
-   (CH0_TRG_VALID,
-    CH1_TRG_VALID,
-    CLK_IN1_D_0_clk_n,
+   (CLK_IN1_D_0_clk_n,
     CLK_IN1_D_0_clk_p,
     EXT_READY,
+    EXT_RESET,
     LED_OUT_0,
     MIXER_VALID,
     adc0_clk_clk_n,
@@ -26,11 +25,10 @@ module design_1_wrapper
     vin0_01_v_p,
     vin0_23_v_n,
     vin0_23_v_p);
-  output CH0_TRG_VALID;
-  output CH1_TRG_VALID;
   input CLK_IN1_D_0_clk_n;
   input CLK_IN1_D_0_clk_p;
   input EXT_READY;
+  input EXT_RESET;
   output LED_OUT_0;
   output MIXER_VALID;
   input adc0_clk_clk_n;
@@ -43,11 +41,10 @@ module design_1_wrapper
   input vin0_23_v_n;
   input vin0_23_v_p;
 
-  wire CH0_TRG_VALID;
-  wire CH1_TRG_VALID;
   wire CLK_IN1_D_0_clk_n;
   wire CLK_IN1_D_0_clk_p;
   wire EXT_READY;
+  wire EXT_RESET;
   wire LED_OUT_0;
   wire MIXER_VALID;
   wire adc0_clk_clk_n;
@@ -61,11 +58,10 @@ module design_1_wrapper
   wire vin0_23_v_p;
 
   design_1 design_1_i
-       (.CH0_TRG_VALID(CH0_TRG_VALID),
-        .CH1_TRG_VALID(CH1_TRG_VALID),
-        .CLK_IN1_D_0_clk_n(CLK_IN1_D_0_clk_n),
+       (.CLK_IN1_D_0_clk_n(CLK_IN1_D_0_clk_n),
         .CLK_IN1_D_0_clk_p(CLK_IN1_D_0_clk_p),
         .EXT_READY(EXT_READY),
+        .EXT_RESET(EXT_RESET),
         .LED_OUT_0(LED_OUT_0),
         .MIXER_VALID(MIXER_VALID),
         .adc0_clk_clk_n(adc0_clk_clk_n),
