09c7dcfa653bfa6d6ad68ee5234da8397188caac
jj1and <kosuke@awa.tohoku.ac.jp>
2020-06-27 16:42:09 +0900

    add debug leds and ILA

--- project_tcl/ZCU111/dataframe_generator_test/design_1_wrapper.v
+++ project_tcl/ZCU111/dataframe_generator_test/design_1_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1.3 (win64) Build 2644227 Wed Sep  4 09:45:24 MDT 2019
-//Date        : Fri Jun 26 18:16:50 2020
-//Host        : AKABEKO03 running 64-bit major release  (build 9200)
+//Date        : Sat Jun 27 15:39:54 2020
+//Host        : Nakamura running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
@@ -12,17 +12,29 @@
 module design_1_wrapper
    (CLK_IN1_D_0_clk_n,
     CLK_IN1_D_0_clk_p,
-    DATAFRAME_GEN_ERROR_0);
+    CLK_LOCKED_LED,
+    DATAFRAME_GEN_ERROR_0,
+    EXT_RESET,
+    RESET_LED);
   input CLK_IN1_D_0_clk_n;
   input CLK_IN1_D_0_clk_p;
+  output CLK_LOCKED_LED;
   output DATAFRAME_GEN_ERROR_0;
+  input EXT_RESET;
+  output [0:0]RESET_LED;
 
   wire CLK_IN1_D_0_clk_n;
   wire CLK_IN1_D_0_clk_p;
+  wire CLK_LOCKED_LED;
   wire DATAFRAME_GEN_ERROR_0;
+  wire EXT_RESET;
+  wire [0:0]RESET_LED;
 
   design_1 design_1_i
        (.CLK_IN1_D_0_clk_n(CLK_IN1_D_0_clk_n),
         .CLK_IN1_D_0_clk_p(CLK_IN1_D_0_clk_p),
-        .DATAFRAME_GEN_ERROR_0(DATAFRAME_GEN_ERROR_0));
+        .CLK_LOCKED_LED(CLK_LOCKED_LED),
+        .DATAFRAME_GEN_ERROR_0(DATAFRAME_GEN_ERROR_0),
+        .EXT_RESET(EXT_RESET),
+        .RESET_LED(RESET_LED));
 endmodule
