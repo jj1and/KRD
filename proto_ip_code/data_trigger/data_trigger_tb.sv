d3fbcfa4620be6347e1d2a09e3a78d4b0cc8b22e
jj1and <kosuke@awa.tohoku.ac.jp>
2020-07-13 21:12:41 +0900

    become to be able to input sample signal to DUT

--- proto_ip_code/data_trigger/data_trigger_tb.sv
+++ proto_ip_code/data_trigger/data_trigger_tb.sv
@@ -1,13 +1,17 @@
 `timescale 1 ps/1 ps
-`include "trigger_config.vh"
+`include "test_signal_gen_pkg.sv"
+import test_signal_gen_pkg::*;
 
 module data_trigger_tb;
+
+    parameter integer RESET_CLK_NUM = 10;
+    parameter integer ACLK_PERIOD = 8000;
+    parameter integer SIGNAL_PERIOD = 8000/`SAMPLE_NUM_PER_CLK;
 
     // these parameter must be configured before logic synthesis
     parameter integer MAX_PRE_ACQUISITION_LENGTH = 2;
     parameter integer MAX_POST_ACQUISITION_LENGTH = 2;
     parameter integer MAX_ADC_SELECTION_PERIOD_LENGTH = 4;
-    parameter integer ADC_SELECTION_PERIOD = 2;
 
     reg ACLK = 1'b0;
     reg ARESET = 1'b0;
@@ -19,11 +23,19 @@ module data_trigger_tb;
     reg H_S_AXIS_TVALID;
 
     // S_AXIS interfrace from L-gain ADC
-    reg [`SAMPLE_WIDTH*2-1:0] L_S_AXIS_TDATA;
+    reg [`LGAIN_TDATA_WIDTH-1:0] L_S_AXIS_TDATA;
     reg L_S_AXIS_TVALID;
 
     // Timestamp
-    reg [`TIMESTAMP_WIDTH-1:0] TIMESTAMP;    
+    reg [`TIMESTAMP_WIDTH-1:0] TIMESTAMP = 0;
+    always @(posedge ACLK) begin
+        if (ARESET) begin
+            TIMESTAMP <= #100 {`TIMESTAMP_WIDTH{1'b1}};
+        end else begin
+            TIMESTAMP <= #100 TIMESTAMP + 1;
+        end
+    end    
+        
 
     // trigger settings
     reg signed [`ADC_RESOLUTION_WIDTH:0] RISING_EDGE_THRSHOLD;
@@ -39,16 +51,125 @@ module data_trigger_tb;
     // h-gain data for charge_sum. timing is syncronized width M_AXIS_TDATA
     wire [`RFDC_TDATA_WIDTH-1:0] H_GAIN_BASELINE_SUBTRACTED_TDATA;
 
+    reg SIG_CLK = 1'b0;
+    reg [$clog2(`SAMPLE_NUM_PER_CLK)-1:0] h_disp_ptr = 0;
+    reg [$clog2(`SAMPLE_NUM_PER_CLK)-1:0] h_subtracted_disp_ptr = 0;
+    reg [$clog2(`LGAIN_SAMPLE_NUM_PER_CLK)-1:0] l_disp_ptr = 0;
+    wire [`SAMPLE_WIDTH-1:0] h_sample = H_S_AXIS_TDATA[h_disp_ptr*`SAMPLE_WIDTH +:`SAMPLE_WIDTH];
+    wire [`SAMPLE_WIDTH-1:0] h_subtracted_sample = H_GAIN_BASELINE_SUBTRACTED_TDATA[h_subtracted_disp_ptr*`SAMPLE_WIDTH +:`SAMPLE_WIDTH];
+    wire [`SAMPLE_WIDTH-1:0] l_sample = L_S_AXIS_TDATA[l_disp_ptr*`SAMPLE_WIDTH +:`SAMPLE_WIDTH];
+    
+    always @(posedge SIG_CLK) begin
+        h_disp_ptr <= #100 h_disp_ptr + 1;
+        l_disp_ptr <= #100 l_disp_ptr + 1;
+        h_subtracted_disp_ptr <= #100 h_subtracted_disp_ptr + 1;
+    end
     
     data_trigger # (
         // these parameter must be configured before logic synthesis
         .MAX_PRE_ACQUISITION_LENGTH(MAX_PRE_ACQUISITION_LENGTH),
         .MAX_POST_ACQUISITION_LENGTH(MAX_POST_ACQUISITION_LENGTH),
-        .MAX_ADC_SELECTION_PERIOD_LENGTH(MAX_ADC_SELECTION_PERIOD_LENGTH),
-        .ADC_SELECTION_PERIOD(ADC_SELECTION_PERIOD)
+        .MAX_ADC_SELECTION_PERIOD_LENGTH(MAX_ADC_SELECTION_PERIOD_LENGTH)
     ) DUT (
         .*
     );
 
+    enum integer {
+        INTITIALIZE,
+        RESET_ALL,
+        CONFIGURING, 
+        NORMAL_SIGNAL_INPUT,
+        SATURATED_SIGNAL_INPUT
+    } test_status;
+
+    task reset_all;
+        test_status = RESET_ALL;
+
+        repeat(RESET_CLK_NUM) @(posedge ACLK);
+        ARESET <= #100 1'b1;
+        @(posedge ACLK);
+        ARESET <= #100 1'b0;
+    endtask
+
+    task config_module( 
+        input int rise_edge_thre, 
+        input int fall_edge_thre, 
+        input int digtal_baseline, 
+        input int pre_acqui_len, 
+        input int post_acqui_len, 
+        input int adc_select_len );
+        
+        test_status = CONFIGURING;
+        // configuring DUT
+        @(posedge ACLK);
+        SET_CONFIG <= #100 1'b1;
+        RISING_EDGE_THRSHOLD <= #100 rise_edge_thre;
+        FALLING_EDGE_THRESHOLD <= #100 fall_edge_thre;
+        DIGITAL_BASELINE <= #100 digtal_baseline;
+        PRE_ACQUISITION_LENGTH <= #100 pre_acqui_len;
+        POST_ACQUISITION_LENGTH <= #100 post_acqui_len;
+        ADC_SELECTION_PERIOD_LENGTH <= #100 adc_select_len;
+
+        @(posedge ACLK);
+        SET_CONFIG <= #100 1'b0;       
+    endtask        
+
+    initial begin
+        ACLK =0;
+        forever #(ACLK_PERIOD/2)   ACLK = ~ ACLK;   
+    end
+
+    initial begin
+        SIG_CLK =0;
+        forever #(SIGNAL_PERIOD/2)   SIG_CLK = ~ SIG_CLK;   
+    end
+
+    task normal_signal_input(input SignalGenerator sample_signal[], input int sample_num);
+        test_status = NORMAL_SIGNAL_INPUT;
+
+        $display("TEST START: input non-satuated signal");
+        @(posedge ACLK);
+        H_S_AXIS_TVALID <= #100 1'b0;
+        L_S_AXIS_TVALID <= #100 1'b0;
+        STOP <= #100 1'b0;
+        fork
+            begin
+                for (int i=0; i<sample_num; i++) begin
+                    h_gain_signal_line_t h_gain_signal_set;
+                    l_gain_signal_line_t l_gain_signal_set;
+                    h_gain_signal_set =  sample_signal[i].getHGainSignalStream();
+                    l_gain_signal_set =  sample_signal[i].getLGainSignalStream();                    
+                    for (int j=0; j<sample_signal[i].total_stream_len; j++) begin
+                        @(posedge ACLK);
+                        H_S_AXIS_TVALID <= #100 1'b1;
+                        H_S_AXIS_TDATA <= #100 h_gain_signal_set[j];
+                        
+                        L_S_AXIS_TVALID <= #100 1'b1;
+                        L_S_AXIS_TDATA <= #100 l_gain_signal_set[j];
+                    end                    
+                end
+            end
+        join
+        $display("TEST PASSED: non-satuated signal test passed!");
+    endtask
+
+    parameter integer SAMPLE_NUM = 10;
+    signal_config_pack signal_config[SAMPLE_NUM];
+    SignalGenerator sample_signal_set[SAMPLE_NUM];
+
+    initial begin
+        test_status = INTITIALIZE;
+        for (int i=0; i<SAMPLE_NUM; i++) begin
+            signal_config[i].rise_time = 5*(i+1);
+            signal_config[i].high_time = 10*(i+1);
+            signal_config[i].fall_time = 20*(i+1);
+            sample_signal_set[i] = new(signal_config[i]);
+            sample_signal_set[i].sampleFilling(2046, 0, 20); // max, baselibe, noise_amp
+        end
+
+        reset_all;
+        config_module(1024, 512, 0, 1, 1, 2); // rise_thre, fall_thre, dig_baseline, pre_acqui_len, post_acqui_len, adc_select_period
+        normal_signal_input(sample_signal_set, SAMPLE_NUM);
+    end
     
 endmodule
--- proto_ip_code/data_trigger/test_signal_gen_pkg.sv
+++ proto_ip_code/data_trigger/test_signal_gen_pkg.sv
@@ -4,57 +4,85 @@
 `include "trigger_config.vh"
 
 package test_signal_gen_pkg;
-    typedef bit [`RFDC_TDATA_WIDTH-1:0] signal_line_t[];
+    typedef bit [`RFDC_TDATA_WIDTH-1:0] h_gain_signal_line_t[];
+    typedef bit [`LGAIN_TDATA_WIDTH-1:0] l_gain_signal_line_t[];
 
-    class Signal_generator;
-        bit signed [`ADC_RESOLUTION_WIDTH-1:0] max_val;
-        bit signed [`ADC_RESOLUTION_WIDTH-1:0] baseline;
+    typedef struct {
+        int rise_time;
+        int high_time;
+        int fall_time;
+    } signal_config_pack;
+
+    class SignalGenerator;
+        int max_val;
+        int baseline;
         int rise_time;
         int high_time;
         int fall_time;
         int total_length;
-        int total_stream_length;
+        int total_stream_len;
         int noise_amp;
-        bit signed [`SAMPLE_WIDTH-1:0] sample_ary[];
+        int sample_ary[];
 
-        function new(input int rise_time, input int high_time, input int fall_time, input noise_amp);
-            this.rise_time = rise_time;
-            this.high_time = high_time;
-            this.fall_time = fall_time;
-            this.noise_amp = noise_amp;
-
-            this.total_length = rise_time + high_time + fall_time;
-            this.total_stream_length = (this.total_length)/`SAMPLE_NUM_PER_CLK;        
+        function new(input signal_config_pack signal_config);
+            this.rise_time = signal_config.rise_time;
+            this.high_time = signal_config.high_time;
+            this.fall_time = signal_config.fall_time;
+            this.total_length = this.rise_time + this.high_time + this.fall_time;
+            this.total_stream_len = this.total_length/`SAMPLE_NUM_PER_CLK;  
         endfunction //new()
 
-        virtual function void sampleFilling(input bit signed [`ADC_RESOLUTION_WIDTH-1:0] max_val, input bit signed [`ADC_RESOLUTION_WIDTH-1:0] baseline);
+        virtual function void sampleFilling(input int max_val, input int baseline, input int noise_amp);
+            int sign;
             this.max_val = max_val;
             this.baseline = baseline;
+            this.noise_amp = noise_amp;
             this.sample_ary = new[this.total_length];
 
             for (int i=0; i<this.rise_time; i++) begin
-                sample_ary[i] = i*(max_val-baseline)/(this.rise_time-1) + baseline;
+                sign = $urandom_range(0, 1);
+                sample_ary[i] = i*(max_val-baseline)/(this.rise_time-1) + baseline + sign*$urandom_range(0, noise_amp/2) - (1-sign)*$urandom_range(0, noise_amp/2);
             end
             for (int i=this.rise_time; i<this.rise_time+this.high_time; i++) begin
-                sample_ary[i] = max_val;
+                sign = $urandom_range(0, 1);
+                sample_ary[i] = max_val + sign*$urandom_range(0, noise_amp/2) - (1-sign)*$urandom_range(0, noise_amp/2);
             end
             for (int i=this.rise_time+this.high_time; i<this.rise_time+this.high_time+this.fall_time; i++) begin
-                sample_ary[i] = max_val - (i-this.rise_time-this.high_time)*(max_val-baseline)/(this.fall_time-1);
+                sign = $urandom_range(0, 1);
+                sample_ary[i] = max_val - (i-this.rise_time-this.high_time)*(max_val-baseline)/(this.fall_time-1) + sign*$urandom_range(0, noise_amp/2) - (1-sign)*$urandom_range(0, noise_amp/2);
             end
         endfunction
 
-        function automatic signal_line_t getSignalStream(input int gain_type);
-            signal_line_t signal_stream;
-            signal_stream = new[total_stream_length];
+        function automatic h_gain_signal_line_t getHGainSignalStream();
+            h_gain_signal_line_t signal_stream;
+            bit [`SAMPLE_WIDTH-1:0] sample_sum;
+            signal_stream = new[this.total_stream_len];
 
-            if (gain_type == 0) begin
-                
-            end else begin
-                
+            for (int i=0; i<this.total_stream_len; i++) begin
+                for (int j=0; j<`SAMPLE_NUM_PER_CLK; j++) begin
+                    signal_stream[i][j*`SAMPLE_WIDTH +:`SAMPLE_WIDTH] = sample_ary[i*`SAMPLE_NUM_PER_CLK+j];
+                end
             end
+            return signal_stream;
         endfunction
 
-    endclass //className
+        function automatic l_gain_signal_line_t getLGainSignalStream();
+            int GAIN_CORRECTION = 20;
+            l_gain_signal_line_t signal_stream;
+            int sample_sum;
+            signal_stream = new[this.total_stream_len];
+            for (int i=0; i<this.total_stream_len; i++) begin
+                for (int j=0; j<`LGAIN_SAMPLE_NUM_PER_CLK; j++) begin
+                    sample_sum = 0;
+                    for (int k=0; k<`SAMPLE_NUM_PER_CLK/`LGAIN_SAMPLE_NUM_PER_CLK; k++) begin
+                        sample_sum += sample_ary[i*`SAMPLE_NUM_PER_CLK+j*(`SAMPLE_NUM_PER_CLK/`LGAIN_SAMPLE_NUM_PER_CLK)+k];
+                    end
+                    signal_stream[i][j*`SAMPLE_WIDTH +:`SAMPLE_WIDTH] = {sample_sum[`SAMPLE_WIDTH] ,sample_sum[`SAMPLE_WIDTH:2]}/GAIN_CORRECTION;
+                end
+            end
+            return signal_stream;
+        endfunction        
+    endclass
 
 endpackage
 `endif
