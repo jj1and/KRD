b7fb9185cf56e55354eae4a0245b71032f735846
jj1and <kosuke@awa.tohoku.ac.jp>
2020-08-18 20:01:04 +0900

    create XSDK project for minimal_FEE

---
+++ XilinxSDK_project/minimal_FEE/minimal_FEE_src/platform_config.h
@@ -0,0 +1,15 @@
+/*
+ * platform_config.h
+ *
+ *  Created on: 2020/05/08
+ *      Author: npart
+ */
+
+#ifndef SRC_PLATFORM_CONFIG_H_
+#define SRC_PLATFORM_CONFIG_H_
+
+#define PLATFORM_ZYNQMP
+#define XPS_ZCU111_BOARD
+#define UART_DEVICE_ID 0
+
+#endif /* SRC_PLATFORM_CONFIG_H_ */
