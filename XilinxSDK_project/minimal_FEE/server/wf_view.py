7853c928fe8067d19458ca2d13d506b749bc7eed
jj1and <kosuke@awa.tohoku.ac.jp>
2020-09-01 22:23:39 +0900

    update viewer

--- XilinxSDK_project/minimal_FEE/server/wf_view.py
+++ XilinxSDK_project/minimal_FEE/server/wf_view.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-BASE_FILE_NAME = "./data/recv_buff_v3_20200827_08.bin"
+BASE_FILE_NAME = "./data/recv_buff_v3_20200901_13.bin"
 
 COMPRESSION_TYPE = 'zip'
 pickle_pddfs_name = BASE_FILE_NAME.replace(
@@ -10,7 +10,7 @@ pickle_pddfs_name = BASE_FILE_NAME.replace(
 npz_waveform_name = BASE_FILE_NAME.replace('.bin', '_waveform.npz')
 
 TIMESTAMP_CLK_Hz = 122.88*1E6
-EFFECTIVE_ADC_CLK_Hz = 0.983*1E9
+EFFECTIVE_ADC_CLK_Hz = TIMESTAMP_CLK_Hz*8
 SAMPLE_PER_TIMESTAMP_CLK = int(EFFECTIVE_ADC_CLK_Hz/TIMESTAMP_CLK_Hz)
 
 if __name__ == "__main__":
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     t0 = pd_dfs['TIMESTAMP'][0]
 
-    for i in np.arange(1000, 1200):
+    for i in (pd_dfs[(pd_dfs['OBJECT_ID'] < 38700) & (pd_dfs['OBJECT_ID'] >= 38500)].index):
         sample_num = pd_dfs['FRAME_LEN'][i]
         wav = waveform_array[i, 0:sample_num]
         t = (pd_dfs['TIMESTAMP'][i]-t0)/TIMESTAMP_CLK_Hz + \
@@ -43,9 +43,29 @@ if __name__ == "__main__":
     ax.legend()
 
     fig2, ax2 = plt.subplots()
-    ax2.set_title("Hit")
+    ax2.set_xscale('symlog')
+    ax2_r = ax2.twinx()
+    ax2.set_title("Hit & Hit-rate")
+
     ax2.set_xlabel("Time[msec]")
     ax2.set_ylabel("total hit num")
-    ax2.plot((pd_dfs['TIMESTAMP']-t0)/TIMESTAMP_CLK_Hz*1e3, pd_dfs.index)
+    ax2.plot((pd_dfs['TIMESTAMP']-t0)/TIMESTAMP_CLK_Hz*1e3,
+             pd_dfs['OBJECT_ID'], label="hit", color='steelblue')
+    ax2.set_yscale('log')
+
+    ax2_r.set_xlabel("Time[msec]")
+    ax2_r.set_ylabel("Hit-rate[MHz]")
+
+    smooth_num = 128
+    smoother = np.ones(smooth_num)/smooth_num
+    smoothed_hit = np.convolve(pd_dfs['OBJECT_ID'], smoother, mode='same')
+    smoothed_t = np.convolve(
+        (pd_dfs['TIMESTAMP']-t0)/TIMESTAMP_CLK_Hz, smoother, mode='same')
+    ax2_r.plot((pd_dfs['TIMESTAMP']-t0)/TIMESTAMP_CLK_Hz*1e3,
+               np.gradient(smoothed_hit, smoothed_t)*1e-6, label="Hit-rate", color='firebrick')
+
+    handler, label = ax2.get_legend_handles_labels()
+    r_handler, r_label = ax2_r.get_legend_handles_labels()
+    ax2.legend(handler+r_handler, label+r_label)
 
     plt.show()
